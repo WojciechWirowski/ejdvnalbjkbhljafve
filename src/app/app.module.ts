1e61b94de1a0427e7f70c2aef598e0ac48c3ac5d
WojciechWirowski <71640545+WojciechWirowski@users.noreply.github.com>
2021-10-12 10:24:14 +0200

    lkjdfsvhsk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetsOverwiewComponent } from './pets-overwiew/pets-overwiew.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetsOverwiewComponent
   ],
   imports: [
     BrowserModule,
